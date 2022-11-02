1828b95f03171211bc2c2c65bea07fe2e9389336
marllonmendez <marlonmendesor@gmail.com>
2022-11-02 19:06:37 -0300

    Second commit

--- game.js
+++ game.js
@@ -1,22 +1,24 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 2.32,
+    scale: 1.7,
     debug: true,
-    clearColor: [0, 0, 0, 1],
+    clearColor: [0, 0, 0, 1]
 })
 
 // Lógica do Jogo
 
+let isJumping = true
+let isBig = false
+
 loadRoot('https://i.imgur.com/')
 
-loadSprite('coin', 'wbKxhcd.png')
-loadSprite('evil-shroom', 'KPO3fR9.png')
-loadSprite('brick', 'pogC9x5.png')
-loadSprite('block', 'M6rwarW.png')
-loadSprite('mario', 'Wb1qfhK.png')
+loadSprite('coin', 'wbKxhcd.png') //moeda
+loadSprite('evil-shroom', 'KPO3fR9.png') //goomba
+loadSprite('brick', 'pogC9x5.png') //tijolo
+loadSprite('block', 'M6rwarW.png') //bloco
 loadSprite('mushroom', '0wMd92p.png')
-loadSprite('surprise', 'gesQ1KP.png')
+loadSprite('surprise', 'gesQ1KP.png') //surpresa
 loadSprite('unboxed', 'bdrLpi6.png')
 loadSprite('pipe-top-left', 'ReTPiWY.png')
 loadSprite('pipe-top-right', 'hj2GK4n.png')
@@ -29,21 +31,82 @@ loadSprite('blue-steel', 'gqVoI2b.png')
 loadSprite('blue-evil-shroom', 'SvV4ueD.png')
 loadSprite('blue-surprise', 'RMqCc1G.png')
 
-    scene("game", ({ score }) => {
+loadSprite('mario', 'OzrEnBy.png', {
+    sliceX: 3.9,
+    anims: {
+        idle: {
+            from: 0,
+            to: 0,
+        },
+        move: {
+            from: 1,
+            to: 2
+        },
+    },
+});
+
+scene("game", ({level, score, big}) => {
         layers(['bg', 'obj', 'ui'], 'obj')
-      
-        const map = [
-            '=                                      =',
-            '=                                      =',
-            '=                                      =',
-            '=                                      =',
-            '=                                      =',
-            '=       %   =*=%=                      =',
-            '=                                      =',
-            '=                            -+        =',
-            '=                  ^   ^     ()        =',
-            '===============================   ======',
-        ]
+    
+        const maps = [
+            [
+                '~                                      ~',
+                '~                                      ~',
+                '~                                      ~',
+                '~                                      ~',
+                '~                                      ~',
+                '~       %   =*=%=                      ~',
+                '~                                      ~',
+                '~                            -+        ~',
+                '~                  ^   ^     ()        ~',
+                '===============================   ======',
+            ],
+            [
+                '/                                      /',
+                '/                                      /',
+                '/                                      /',
+                '/                                      /',
+                '/                                      /',
+                '/                                      /',
+                '/     %!%                              /',
+                '/                       x x            /',
+                '/                     x x x x    -+    /',
+                '/            z   z  x x x x x x  ()    /',
+                '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+            ],
+            [
+                '                                        ',
+                '                             !          ',
+                '                            %%%%%%      ',
+                '                     !                  ',
+                '             %%%    %%%%%               ',
+                '      %%%                               ',
+                '                                        ',
+                '                                        ',
+                '     !    !   =  ^  ^     =    =         ',
+                '===========================    =========/',
+                '                          =    =        /',
+                '                                        /',
+                '        !                               /',
+                '      %*%                               /',
+                ' -+                                     /',
+                ' ()!         !                         z/',
+                '!!!!!!!!!!!!!!!!!    !!!!!!!!!!!!!!!!!!!!!',
+              ],
+              [
+                '~                                       ~',
+                '~                                       ~',
+                '~                                       ~',
+                '~                                       ~',
+                '~                                       ~',
+                '~      ==%==          ~                ~',
+                '~                   ~  ~  ~             ~',
+                '~                ~  ~  ~  ~  ~   -+     ~',
+                '~                ~  ~  ~  ~  ~   ()     ~',
+                '=========================================',
+              ],
+
+    ]
 
     const levelConfig = {
         width: 20,
@@ -53,31 +116,31 @@ loadSprite('blue-surprise', 'RMqCc1G.png')
         '%': [sprite('surprise'), solid(), 'coin-surprise'],
         '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
         '}': [sprite('unboxed'), solid()],
+        '#': [sprite('mushroom'), body(), 'mushroom'],
+        '^': [sprite('evil-shroom'), 'dangerous'],
+        '~': [sprite('brick'), solid()],
         '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
         ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
         '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
         '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
-        '^': [sprite('evil-shroom'), solid(), 'dangerous'],
-        '#': [sprite('mushroom'), solid(), 'mushroom', body()],
         '!': [sprite('blue-block'), solid(), scale(0.5)],
-        '£': [sprite('blue-brick'), solid(), scale(0.5)],
-        'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
-        '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
+        '/': [sprite('blue-brick'), solid(), scale(0.5)],
+        'z': [sprite('blue-evil-shroom'), body(), scale(0.5), 'dangerous'],
         'x': [sprite('blue-steel'), solid(), scale(0.5)],
     }
 
-    const gameLevel = addLevel(map, levelConfig)
+    const gameLevel = addLevel(maps[level], levelConfig)
 
     const scoreLabel = add([
         text('Moedas: ' +score, 10),
         pos(30, 6),
         layer('ui'),
         {
-          value: score
+        value: score
         }
-      ])
-    
-    //   add([text('level ' + parseInt(level + 1) ), pos(40, 6)])
+    ])
+
+    add([text('Level: ' +parseInt(level + 1), 10), pos(30,30)])
 
     function big(){
         return{
@@ -96,18 +159,25 @@ loadSprite('blue-surprise', 'RMqCc1G.png')
     }
 
     const player = add([
-        sprite('mario'),
+        sprite('mario', {
+        animSpeed: 0.1,
+        frame: 0  
+        }),
         solid(),
         pos(50, 0),
         body(),
         big(),
-        origin('bot')
+        origin('bot'),
+        {
+            speed: 120
+        }
     ])
 
-    action('mushroom', (obj) => {
-        obj.move(20,0)
-    })
+    if(isBig){
+        player.biggify()
+    }
 
+    //Animação do Mario
     keyDown('left', () => {
         player.flipX(true)
         player.move(-120,0)
@@ -119,11 +189,34 @@ loadSprite('blue-surprise', 'RMqCc1G.png')
         player.move(120,0)
     })
 
+    keyRelease('left', () =>{
+        player.play('idle')
+    })
+
+    keyRelease('right', () =>{
+        player.play('idle')
+    })
+
     keyPress('space', () => {
         if(player.grounded()){
             player.jump(400)
             isJumping = true
         }
+    })
+
+    keyPress('left', () => {
+        player.flipX(true)
+        player.play('move')
+    })
+
+    keyPress('right', () => {
+        player.flipX(false)
+        player.play('move')
+    })
+
+    //Ações
+    action('mushroom', (obj) => {
+        obj.move(20,0)
     })
 
     action('dangerous', (obj) => {
@@ -151,6 +244,7 @@ loadSprite('blue-surprise', 'RMqCc1G.png')
         }
     })
 
+    //Colisões
     player.collides('mushroom', (obj) => {
         destroy(obj)
         player.biggify()
@@ -159,26 +253,36 @@ loadSprite('blue-surprise', 'RMqCc1G.png')
     player.collides('dangerous', (obj) => {
         if(isJumping){
             destroy(obj)
+        }else if(isBig){
+            player.smallify()
         }else{
-            if(isBig){
-                player.smallify()
-            }else{
-                go("lose")
-            }
+            go("lose", ({score: scoreLabel.value}))
         }
+    })  
+
+    player.collides('coin', (obj) => {
+        destroy(obj)
+        scoreLabel.value++
+        scoreLabel.text = 'Moedas: ' +scoreLabel.value
+    })
+
+    player.collides('pipe', () => {
+        keyPress('down', () =>{
+            go("game", {
+                level: (level + 1) % maps.length,
+                score: scoreLabel.value,
+                Big: isBig
+            })
+        })
     })
 
 })
 
-player.collides('coin', (obj) => {
-    destroy(obj)
-    scoreLabel.value++
-    scoreLabel.text = 'Moedas: ' +scoreLabel.value
-})
-
-
 scene("lose", ({ score }) => {
-    add([text('score: ' +score, 18), origin('center'), pos(width()/2, height()/2)])
+add([text('score: ' +score, 18), origin('center'), pos(width()/2, height()/2)])
+    keyPress('space', () => {
+    go("game", {level: 0, score: 0, big: isBig})
+    })
 })
 
-go("game", { score: 0 })
+go("game", ({level: 0, score: 0, big: isBig}))
